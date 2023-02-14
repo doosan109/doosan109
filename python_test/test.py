69316f57b16980ad81e35086b453b8ee5561f18e
doosan109 <doosan109@naver.com>
2023-02-13 18:04:12 -0800

    bb

---
+++ python_test/test.py
@@ -0,0 +1,31 @@
+
+class Test:
+
+ def __init__(self):
+    self.var = 10
+    self.var2 = 10
+
+    def aa(self):
+        print('aaaabb')
+
+
+
+class Inh(Test):
+    def__init__(self)
+    super(). __init__()
+    
+def main():
+
+    a = 10
+    a = "hello"
+    b = int()
+    print(a)
+    ob = Test
+    ob2 = Test
+    inob = Inh()
+    ob.aa()
+    ob.bb()
+    print(ob.var)
+    print(ob.var2)
+    print(ob2.var)
+    prinf(ob2.var2)
